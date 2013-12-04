84598ed4c5dca0de240e4723dcf2187f73449812
seanfred <sean.frederick.1@gmail.com>
2013-12-04 13:17:14 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Implementing BDD using Minitest and writing specs.
---
+++ spec/welcome_spec.rb
@@ -0,0 +1,11 @@
+require "minitest/autorun"
+require "minitest/spec"
+
+require "welcome"
+
+describe Welcome do
+	it "has a message" do
+		hello = Welcome.new
+		hello.message.must_match "Welcome"
+	end
+end
